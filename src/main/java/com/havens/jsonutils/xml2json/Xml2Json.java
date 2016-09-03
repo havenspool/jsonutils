c0650697f1c42c3f0f70546597b4a7f802f8ef95
havens <havenspool@gmail.com>
2016-09-03 23:54:39 +0800

    test

--- src/main/java/com/havens/jsonutils/xml2json/Xml2Json.java
+++ src/main/java/com/havens/jsonutils/xml2json/Xml2Json.java
@@ -3,20 +3,27 @@ package com.havens.jsonutils.xml2json;
 
 import java.io.File;
 import java.util.List;
-
 /**
  * @desc: TODO
  * @author: xuwenwu
  * @date: 2016/9/3 11:45
  */
 public class Xml2Json {
-    public static void main(String[] args) throws Exception{
-//        XmlMapper xmlMapper = new XmlMapper();
-//        List entries = xmlMapper.readValue(new File("input.xml"), List.class);
+//    private static final String STR_JSON = "{\"name\":\"Michael\",\"address\":{\"city\":\"Suzou\",\"street\":\" Changjiang Road \",\"postcode\":100025},\"blog\":\"http://www.ij2ee.com\"}";
+//    public static String xml2JSON(String xml){
+//        return new XMLSerializer().read(xml).toString();
+//    }
 //
-//        ObjectMapper jsonMapper = new ObjectMapper();
-//        String json = jsonMapper.writeValueAsString(entries);
-//        System.out.println(json);
-        // [{"name":"AXL","time":"19-07","price":"11.34"},{"name":"AIK","time":"19-07","price":"13.54"},{"name":"ALO","time":"19-07","price":"16.32"},{"name":"APO","time":"19-07","price":"13.56"}]
+//    public static String json2XML(String json){
+//        JSONObject jobj = JSONObject.fromObject(json);
+//        String xml =  new XMLSerializer().write(jobj);
+//        return xml;
+//    }
+
+    public static void main(String[] args) {
+//        String xml = json2XML(STR_JSON);
+//        System.out.println("xml = "+xml);
+//        String json = xml2JSON(xml);
+//        System.out.println("json="+json);
     }
 }
