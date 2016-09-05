4e77adedc9585d29938d0183cef5aad54ee41a2e
havenspool <havenspool@gmail.com>
2016-09-05 18:35:45 +0800

    pom.xml

---
+++ src/main/java/com/havens/jsonutils/csv2json/Csv2Json.java
@@ -0,0 +1,33 @@
+package com.havens.jsonutils.csv2json;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.MappingIterator;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvParser;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+
+import java.io.File;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+/**
+ * @desc: TODO
+ * @author: xuwenwu
+ * @date: 2016/9/5 17:53
+ */
+public class Csv2Json {
+    public static void main(String[] args) throws Exception{
+        String fileName="E:\\git_code\\jsonutils\\src\\main\\resources\\handbookLv.csv";
+        CsvMapper mapper = new CsvMapper();
+        mapper.enable(CsvParser.Feature.WRAP_AS_ARRAY);
+        File csvFile = new File(fileName); // or from String, URL etc
+        MappingIterator<String[]> it = mapper.readerFor(String[].class).readValues(csvFile);
+        while (it.hasNext()) {
+            String[] row = it.next();
+            System.out.println(Arrays.toString(row));
+            // and voila, column values in an array. Works with Lists as well
+        }
+    }
+}
--- src/main/java/com/havens/jsonutils/xml2json/Xml2Json.java
+++ src/main/java/com/havens/jsonutils/xml2json/Xml2Json.java
@@ -1,8 +1,11 @@
 package com.havens.jsonutils.xml2json;
 
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
 import java.io.File;
-import java.util.List;
 
 /**
  * @desc: TODO
@@ -11,12 +14,18 @@ import java.util.List;
  */
 public class Xml2Json {
     public static void main(String[] args) throws Exception{
-//        XmlMapper xmlMapper = new XmlMapper();
-//        List entries = xmlMapper.readValue(new File("input.xml"), List.class);
-//
-//        ObjectMapper jsonMapper = new ObjectMapper();
-//        String json = jsonMapper.writeValueAsString(entries);
-//        System.out.println(json);
-        // [{"name":"AXL","time":"19-07","price":"11.34"},{"name":"AIK","time":"19-07","price":"13.54"},{"name":"ALO","time":"19-07","price":"16.32"},{"name":"APO","time":"19-07","price":"13.56"}]
+        String fileName="E:\\git_code\\jsonutils\\src\\main\\resources\\CardType.xml";
+        ObjectMapper m = new ObjectMapper();
+        // can either use mapper.readTree(source), or mapper.readValue(source, JsonNode.class);
+        JsonNode rootNode = m.readTree(new File(fileName));
+        // ensure that "last name" isn't "Xmler"; if is, change to "Jsoner"
+        JsonNode nameNode = rootNode.path("name");
+        String lastName = nameNode.path("last").asText();
+        System.out.println(lastName);
+        if ("xmler".equalsIgnoreCase(lastName)){
+            ((ObjectNode) nameNode).put("last", "Jsoner");
+        }
+        // and write it out:
+        m.writeValue(new File("user-modified.json"), rootNode);
     }
 }
